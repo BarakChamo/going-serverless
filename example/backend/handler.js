4df2c1f9937c0bfe1474710fcf9027fb99b81e52
Barak Chamo <barakchamo@gmail.com>
2016-08-22 14:17:08 -0400

    updates

--- example/backend/handler.js
+++ example/backend/handler.js
@@ -1,19 +1,39 @@
 /* jshint esversion:6, node:true, asi:true */
 'use strict'
 
-const aws = require('aws-sdk')
+const AWS = require('aws-sdk')
 const doc = require('dynamodb-doc')
 const https = require('https')
 
-const s3 = new aws.S3({ apiVersion: '2006-03-01' })
-const dynamo = new doc.DynamoDB()
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
+const dynamo = new AWS.DynamoDB.DocumentClient()
 
-function processRow(row){
-    console.log(row)
+const DYNAMO_MAX_REQS = 25
+
+function processRow(row) {
+  const data = row.split(',')
+
+  return {
+    id: data[0],
+    color: data[1],
+    size: +data[2]
+  }
+}
+
+function putItems(items) {
+  // Map items to dynamodb sdk format
+  const puts = items.map(item => ({PutRequest: {Item: item}}))
+
+  // Promisify DynamoDB's callback
+  return new Promise((resolve, reject) => {
+      // Create a batched DynamoDB put request
+      dynamo.batchWrite({
+        RequestItems: { SomeTable: puts }
+      }, (err, data) => err ? reject(err) : resolve(data))
+  })
 }
 
 exports.handler = (event, context, callback) => {
-    console.log(JSON.stringify(event));
   // Get the S3 bucket and key from the event
   const bucket = event.Records[0].s3.bucket.name
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
@@ -21,15 +41,29 @@ exports.handler = (event, context, callback) => {
   // Retrieve updated object from bucket
   s3.getObject({Bucket: bucket, Key: key}, (err, data) => {
       if (err) {
-        // Failed to retrieve stock file
-        console.log('ERROR',err)
         context.fail(err)
-        callback(err)
       } else {
-        // Parse CSV rows
-        data.Body.toString().split(/\r?\n/).forEach(processRow)
+        const ops = []
 
-        // callback(null, 'products')
+        // Parse CSV rows into data items
+        const items = data.Body.toString().split(/\r?\n/)
+
+        // Remove empty tail
+        items.pop()
+
+        // Split items by max requests allowed in batch DynamoDB operations
+        for (var i = 0; i < items.length; i += DYNAMO_MAX_REQS)
+          ops.push(
+            // Map each row into a DynamoDB items
+            items.slice(i, i + DYNAMO_MAX_REQS).map(processRow)
+          )
+
+        // Store data items in dynamodb table
+        const requests = ops.map(putItems)
+
+        Promise.all(requests)
+          .then(data => context.succeed(data))
+          .catch(error => context.fail(err))
       }
   })
 }
